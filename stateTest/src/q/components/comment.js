760ec99f1ed9bb98737b20fe20154fe76f0b72ac
parkadew <pkm_design@naver.com>
2024-06-29 10:56:44 +0900

    6/29

--- stateTest/src/q/components/comment.js
+++ stateTest/src/q/components/comment.js
@@ -1,24 +1,43 @@
 import { useState } from "react";
 import styled from "styled-components";
 
-function Comment({ post, setpost }) {
+function Comment({ post, setPost }) {
+
+  const IdPost = post.Comments.map((l) => ({
+    ...l, id: Math.floor(Math.random() * 10000)
+
+  }));
+  console.log(IdPost)
 
 
 
-
+  // Idpost를 사용할때 IdPost[0]를 사용하지 않으면 그 배열로 들어가지 못한다 
+  // 헌데 왜 밑에서 map을 돌린 IdPost.map은 매개변수로 받은 co를 바로 복사본으로 받아볼수있는것일까??
+  // map이 복사본 배열을 반환한다...
+  // 그럼
+  // 내가 왜 한번 더 map을 돌렸을까
+  // 전부다 순회시켜서 요소들을 가져와야하기때문이야 -> 왜 그럼 foreach가 아니라 map으로 가져온것일까
+  // 만약 map을 돌려서 가져오게 되면 내가 설정한 co에 어떤게 어게 되는걸까 
+  // co는 콜백함수의 매개변수로 새로운 수노히를 함으로써 새로운 복사본 배열이 오게된것일것이다.
+  // 내가 IdPost에서는 이미 Posts.Comments 로 들어가서 map을 돌렸기 때문에 Comments내의 정보 배열만 오게되었을것이다
+  // 내가 넣어준 l은 복사본이야 IdPost 의! Posts.Comments의 값을 가져온 복사본이라고
+  // 밑에 생성된 map은 co라는 매개변수의 요소에 접근되는것이고
+  // 그그 {} 안에 들어가있잖아
+  // 
+  //그래 내가 복사본을 생성해서 
 
   return (
-    <>
-      {post.Comments.map((co) => <S.CommentItem key={co.User}>
+    <S.CommentItem>
+      {IdPost.map((co) => <form>
         <p>
           작성자: <span>{co.User.nickname}</span>
         </p>
         <p>
           댓글 내용: <span>{co.content}</span>
         </p>
-      </S.CommentItem >)
-      }
-    </>
+        <button>삭제</button>
+      </form>)}
+    </S.CommentItem>
   );
 }
 export default Comment;
--- stateTest/src/q/state21.js
+++ stateTest/src/q/state21.js
@@ -14,7 +14,6 @@ function State2() {
             2. 댓글 수정 기능
             3. 댓글 삭제 기능 ( 본인이 작성한 댓글만 삭제할 수 있습니다, myComment 활용 )
     */
-
   const [post, setPost] = useState({
     title: "안녕하세요 여러분 김성용 강사입니다 :)",
     content: "오늘도 모두 화이팅입니다!",
@@ -32,6 +31,7 @@ function State2() {
         myComment: false,
       },
       {
+
         User: {
           nickname: "반하나",
         },
@@ -39,6 +39,7 @@ function State2() {
         myComment: false,
       },
       {
+
         User: {
           nickname: "오렌지",
         },
@@ -46,6 +47,7 @@ function State2() {
         myComment: false,
       },
       {
+
         User: {
           nickname: "이멜론",
         },
@@ -62,35 +64,14 @@ function State2() {
     ],
   });
 
-  // 댓글 입력 데이터 설정
-  const [Commentli, setCommentli] = useState({
-    nickname: "",
-    content: "",
-  });
-  // 입력 필드 변경 핸들러
-  // 사용자가 입력 필드에 값을 입력할 때 호출되며, commentData 상태를 업데이트
-  const onPressPlusComment = (e) => {
-    const { name, value } = e.target;
-    // [name]:value를 통해서 input안의 name의 value를 찾고, 그것을 새롭게 들어오는 e.target.value로 변경해줄 수 있다.
-    setCommentli({ ...Comment, [name]: value });
-  };
-  // const [NewWriter, setNewWriter] = useState("")
-  // const [NewComment, setNewComment] = useState("")
+  const idpost = post.Comments.map((l) => ({
+    ...l, id: Math.floor(Math.random() * 10000)
+  }))
 
-  // const onPressPlusComment = (event) => {
-  //   event.preventDefault();
-  //   const NewCList = post.Comments.map((c) => ({
-  //     ...c,
-  //     id: Math.floor(Math.random() * 10000000),
-  //     User: {
-  //       nickname: NewWriter,
-  //     },
-  //     content: NewComment,
-  //     myComment: false,
-  //   }))
 
-  //   setPost([...post.Comments, NewCList])
-  // }
+
+
+
   return (
     <S.Wrapper>
       <h1>문제2</h1>
@@ -113,14 +94,22 @@ function State2() {
         <p>
           댓글 수: <span>{post.Comments.length}</span>
         </p>
-        <input name="nickname" value={Commentli.nickname} placeholder="작성자" />
-        <input name="content" value={Commentli.content} placeholder="댓글 내용" />
-        <button onClick={onPressPlusComment}>댓글 작성</button>
+        <input name='Writer' placeholder="작성자" />
+        <input name='comment' placeholder="댓글 내용" />
+        <button >댓글 작성</button>
       </div>
       <S.CommentList>
-        <Comment post={post} setpost={setPost} />
+        {post.Comments.map((co) => <div key={co.User}>
+          <p>
+            작성자: <span >{co.User.nickname}</span>
+          </p>
+          <p>
+            댓글 내용: <span >{co.content}</span>
+          </p>
+        </div >)
+        }
       </S.CommentList>
-    </S.Wrapper>
+    </S.Wrapper >
   );
 }
 export default State2;
@@ -146,7 +135,6 @@ const PostTitle = styled.p`
 const PostContent = styled.p`
   color: #fff;
 `;
-
 const PostInfo = styled.div`
   width: 360px;
   border: 3px solid #f00;
