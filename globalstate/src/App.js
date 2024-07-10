d5ae40b1e946af63f35da14c21a7f109ed4c2108
parkadew <pkm_design@naver.com>
2024-07-10 14:45:57 +0900

    7/10

--- globalstate/src/App.js
+++ globalstate/src/App.js
@@ -1,16 +1,24 @@
-
+import logo from './logo.svg';
 import './App.css';
-import AddTodoModal from './components/addtodoModal';
-import TodoList from './components/todolist';
-import TodoProvider from './context/todo.context';
 
 function App() {
   return (
-    <TodoProvider>
-      {/*이 안에 있는 컴포너트들은 내가 만든 전역 상태(내가 만든 전역상태가 todo.context겠지)를 공유할 수있어요 */}
-      <AddTodoModal></AddTodoModal>
-      <TodoList></TodoList>
-    </TodoProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- globalstate/src/reducer/user.reducer.js
+++
@@ -1,9 +0,0 @@
-export const userReducer = (state, action) => {
-    switch (action.type) {
-        case 'ADD_USER':
-            return [...state, action.payload];
-
-        case 'DELETE_USER':
-            return state.filter((user) => user.id !== action.payload.id)
-    }
-};
