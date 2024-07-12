2358fa481c425977c63057a7297525e21ba50aa1
parkadew <pkm_design@naver.com>
2024-07-12 17:15:55 +0900

    7/16

--- globalstate/src/components/addTodoModal.jsx
+++ globalstate/src/components/addTodoModal.jsx
@@ -18,4 +18,6 @@ const AddTodoModal = () => {
 export default AddTodoModal
 
 //전역 변수를 사용하려면 UseContext를 사용해주어야한다
+// hookstodo를 전역상태 관리 해보자
+// ---useReducer 이거 꼭!꼭 제대로 알고넘어가자 복잡한 상태 변경이나, 비즈니스 로직을 재사용
 
