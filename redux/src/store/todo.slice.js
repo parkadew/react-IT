dec465397854417036cc6dbc45b26c6facafdfb3
parkadew <pkm_design@naver.com>
2024-07-26 12:04:16 +0900

    7/26

--- redux/src/store/todo.slice.js
+++ redux/src/store/todo.slice.js
@@ -1,3 +1,4 @@
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     todo: [
@@ -8,12 +9,15 @@ const initialState = {
         },
     ],
 };
+// 기본값인데 없으면 빈배열을 주면된다
 
 export const todoSlice = createSlice({
     name: "todo",
     reducers: {
         addTodo: (state, action) => {
             state.unshift(action.payload);
+            // 이게 ...을 쓰지않아도 할수있게끔해준다 익명 함수에 state,action을 받으면 된다
+            //  action.payload는 새로운 todo 항목
         },
         deleteTodo: (state, action) => {
             state.todo = state.todo.filter((todo) => todo.id !== action.payload);
@@ -24,6 +28,6 @@ export const todoSlice = createSlice({
         },
     },
 })
-
+// name은 todo -> 키 값이다.
 export const { addTodo, deleteTodo, updateTodo } = todoSlice.action;
 export default todoSlice.reducer;
